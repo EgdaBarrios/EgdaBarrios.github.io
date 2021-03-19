d4466a8712390f76dcd0a47c04264e90536f5b5c
Egda <egdabarrios@gmail.com>
2021-03-19 16:53:17 -0500

    'deploy'

---
+++ precache-manifest.37871a39e000bdc57db84038cdbd2d13.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72129bd5749b6687dc7",
+    "url": "/css/app.0ea9db70.css"
+  },
+  {
+    "revision": "0600380dd21c50baf7ae2bd501268a2e",
+    "url": "/img/arrowsUp.0600380d.png"
+  },
+  {
+    "revision": "058fb1eb7766483312d2351e477d83a2",
+    "url": "/img/breakfast.058fb1eb.png"
+  },
+  {
+    "revision": "19f45c41e03ee9b53a94a8d7df70e80f",
+    "url": "/img/burgerqueen_tablet.19f45c41.png"
+  },
+  {
+    "revision": "03682ab4d52ec7c0e9dc4805a8e9acff",
+    "url": "/img/cosplay_laptop.03682ab4.png"
+  },
+  {
+    "revision": "cad8641085026b060dd3bbf11b67154f",
+    "url": "/img/feed.cad86410.png"
+  },
+  {
+    "revision": "d8ec9fbe13956e3e278fb7d929cc1ecc",
+    "url": "/img/mobile.d8ec9fbe.png"
+  },
+  {
+    "revision": "6ed138cc98eb5604bb2e4ac62a8f63e2",
+    "url": "/img/mobileCp1.6ed138cc.png"
+  },
+  {
+    "revision": "0bad08910aebea878344fe63aeae1505",
+    "url": "/img/mobileCp2.0bad0891.png"
+  },
+  {
+    "revision": "a5fb21f52044d6fb03d94881856b6377",
+    "url": "/img/otherMeals.a5fb21f5.png"
+  },
+  {
+    "revision": "f8dbafb17fb6b1964db355368ebe1e37",
+    "url": "/img/pointing_up.f8dbafb1.gif"
+  },
+  {
+    "revision": "5ec2dd57cdbbe2eadb101a114e218056",
+    "url": "/img/pokedex_laptop.5ec2dd57.png"
+  },
+  {
+    "revision": "87ef3613f67a38e839fb73f032291595",
+    "url": "/img/portfolioPhoto.87ef3613.png"
+  },
+  {
+    "revision": "b25900496450cdd0f6d170816c56bf7d",
+    "url": "/img/profile.b2590049.png"
+  },
+  {
+    "revision": "fd49887f894712f5e2991f31a9b5a489",
+    "url": "/img/prototipoBaja.fd49887f.png"
+  },
+  {
+    "revision": "ac9ec70fde80a44ff59e3c64db34b6e7",
+    "url": "/img/register-log.ac9ec70f.png"
+  },
+  {
+    "revision": "aaa86ea423ae988191a455af9f834644",
+    "url": "/img/restaurant.aaa86ea4.png"
+  },
+  {
+    "revision": "d385ad529f85e215e36d68a5f9a12342",
+    "url": "/img/tablet1.d385ad52.png"
+  },
+  {
+    "revision": "392d9cedb2c46c9b98ebb59770998c3e",
+    "url": "/img/tablet2.392d9ced.png"
+  },
+  {
+    "revision": "95cfcf9d52b55fb1f3dad640814b583d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f72129bd5749b6687dc7",
+    "url": "/js/app.b949d9ae.js"
+  },
+  {
+    "revision": "c7d235abcf0e664a9d60",
+    "url": "/js/chunk-vendors.c2fc1461.js"
+  },
+  {
+    "revision": "393ed8967b2ebc0f947c2ae90e45edb3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "baa405a556d409e4287ff95d50a38a5c",
+    "url": "/media/COSplay.baa405a5.mp4"
+  },
+  {
+    "revision": "13acec951b28914dd71eba46e8e96b51",
+    "url": "/media/burger-queen.13acec95.mp4"
+  },
+  {
+    "revision": "f10e914720e8654966e8b24e1aeffa14",
+    "url": "/media/poke-cp.f10e9147.mp4"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
